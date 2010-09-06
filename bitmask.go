44409b6722ee2e5600115d6f141624e05cd63f6b
Krzysztof Goj <krzysztof.goj@erlang-solutions.com>
2010-09-07 01:42:11 +0200

    Basic functionality

--- bitmask.go
+++ bitmask.go
@@ -5,9 +5,10 @@ import "fmt"
 type part uint64
 const sz = 64
 const msk = sz-1
+const sft = 6 // sz = 1 << sft
 
 type Bitmask struct {
-    x, y int
+    X, Y int
     w, h int
     lines [][]part
 }
@@ -15,11 +16,11 @@ type Bitmask struct {
 func FromString(x, y, w, h int, str string) Bitmask {
     result := Bitmask{x, y, w, h, make([][]part, h)}
     for i, r := uint(0), 0; r<h; r++ {
-        row := make([]part, w / sz + 1)
+        row := make([]part, w >> sft + 1)
         result.lines[r] = row
         for c := 0; c < w; c++ {
             if str[i] != '.' && str[i] != ' ' {
-                row[c / sz] |= part(1 << uint(c % sz))
+                row[c >> sft] |= part(1 << uint(c & msk))
             }
             i++
         }
@@ -27,11 +28,21 @@ func FromString(x, y, w, h int, str string) Bitmask {
     return result
 }
 
+func (b Bitmask) SetRel(x, y int, val bool) {
+    if (y < 0 || y >= b.h) {return}
+    if (x < 0 || x >= b.w) {return}
+    if val {
+        b.lines[y][x >> sft] |=  part(1 << uint(x & msk))
+    } else {
+        b.lines[y][x >> sft] &^= part(1 << uint(x & msk))
+    }
+}
+
 func (b Bitmask) Format(f fmt.State, c int) {
-    for r := 0; r<b.h; r++ {
-        row := b.lines[r]
-        for c := 0; c < b.w; c++ {
-            if row[c / sz] & part(1 << uint(c % sz)) != 0 {
+    fmt.Fprintf(f, "\nBM+(%d, %d):\n", b.X, b.Y)
+    for y := 0; y<b.h; y++ {
+        for x := 0; x < b.w; x++ {
+            if b.rel(x, y) {
                 fmt.Fprint(f, "#")
             } else {
                 fmt.Fprint(f, ".")
@@ -41,6 +52,45 @@ func (b Bitmask) Format(f fmt.State, c int) {
     }
 }
 
-func Collides(b1 Bitmask, b2 Bitmask) bool {
-    return true
+func (b1 Bitmask) Collides (b2 Bitmask) bool {
+    dx, dy := b2.X - b1.X, b2.Y - b1.Y
+    if (dx < 0) {return b2.Collides(b1)}
+    miny, maxy := max(-dy, 0), min(b1.h - dy, b2.h)
+    mini, maxi := max(-dx >> sft, 0), min((b1.w-dx) >> sft + 1, b2.w >> sft + 1)
+    di, shift1, shift2 := part(dx >> sft), part(dx & msk), sz - part(dx & msk)
+    for y := miny; y < maxy; y++ {
+        for i := part(mini); i < part(maxi); i++ {
+            bits := b1.lines[y + dy][i + di]
+            if i>0 && bits << shift2 & b2.lines[y][i-1] != 0 { return true }
+            if        bits >> shift1 & b2.lines[y][i]   != 0 { return true }
+        }
+    }
+    return false
+}
+
+// wtf, wtf, wtf...
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+// private functions for testing - don't have to be fast
+
+func (b Bitmask) abs(x, y int) bool {
+    return b.rel(x - b.X, y - b.Y)
+}
+
+func (b Bitmask) rel(x, y int) bool {
+    if (y < 0 || y >= b.h) {return false}
+    if (x < 0 || x >= b.w) {return false}
+    return b.lines[y][x >> sft] & part(1 << uint(x & msk)) != 0
 }
--- bitmask_test.go
+++ bitmask_test.go
@@ -1,10 +1,124 @@
 package bitmask
 
 import (
+        "fmt"
+        "rand"
+        "reflect"
         "testing"
+        "testing/quick"
 )
 
-func TestThatFails(t *testing.T) {
-    bmsk := FromString(0, 0, 3, 3, ".x.xxx.x.")
-    t.Error(bmsk)
+type exp_t map[int][]bool;
+
+func TestAbsRelExplicitData(t *testing.T) {
+    bmsk := FromString(4, -3, 3, 3, ".#."+
+                                    "#.#"+
+                                    ".##")
+    expectations := exp_t { // [y][x] -> t/f
+        0: []bool {false,  true, false, /* out of bounds */ false},
+        1: []bool { true, false,  true},
+        2: []bool {false,  true,  true},
+        // out of bounds
+        3: []bool {false,  false},
+    }
+    doAbsRelTest(bmsk, expectations, t)
 }
+
+func TestCollidesEdgeCase(t *testing.T) {
+    b1 := FromString(0, 0, 65, 1, "................................................................#")
+    b2 := FromString(1, 0, 64, 1, "...............................................................#")
+    if !b1.Collides(b2) {
+        t.Error(b1, b2)
+    }
+}
+
+func TestDumbCollidesOneElem(t *testing.T) {
+    testCollidesOneElem(func(b1, b2 Bitmask) bool {return b1.dumbCollides(b2)}, t)
+}
+
+func TestCollidesOneElem(t *testing.T) {
+    testCollidesOneElem(func(b1, b2 Bitmask) bool {return b1.Collides(b2)}, t)
+}
+
+func TestCollides(t *testing.T) {
+    f := func(b1 Bitmask, b2 Bitmask) bool {
+        return b1.Collides(b2) == b1.dumbCollides(b2)
+    }
+    if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// helper functions
+
+func (b Bitmask) Generate(rand *rand.Rand, size int) reflect.Value {
+    result := Bitmask {
+        X: size - rand.Intn(size),
+        Y: size - rand.Intn(size),
+        w: rand.Intn(size),
+        h: rand.Intn(size),
+    }
+    result.lines = make([][]part, result.h)
+    for y := 0; y<result.h; y++ {
+        result.lines[y] = make([]part, result.w / sz + 1)
+        for x := 0; x < result.w; x++ {
+            result.SetRel(x, y, rand.Intn(size/4) == 0)
+        }
+    }
+
+    return reflect.NewValue(result)
+}
+
+func doAbsRelTest(bmsk Bitmask, expectations exp_t, t *testing.T) {
+    for y, yexpect := range expectations {
+        for x, val := range yexpect {
+            if (val != bmsk.abs(x + bmsk.X, y + bmsk.Y)) {
+                t.Error(x + bmsk.X, y + bmsk.Y, bmsk, "abs should be", val)
+            }
+            if (val != bmsk.rel(x, y)) {
+                t.Error(x, y, bmsk, "rel should be", val)
+            }
+        }
+    }
+}
+
+func (b1 Bitmask) dumbCollides(b2 Bitmask) bool {
+    for y := 0; y < b1.h; y++ {
+        for x := 0; x < b1.w; x++ {
+            if b1.abs(b1.X + x, b1.Y + y) && b2.abs(b1.X + x, b1.Y + y) {
+                return true;
+            }
+        }
+        for y := 0; y < b2.h; y++ {
+            for x := 0; x < b2.w; x++ {
+                if b1.abs(b2.X + x, b2.Y + y) && b2.abs(b2.X + x, b2.Y + y) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+func staticallyAssertBitmaskIsGenerator(t *testing.T) quick.Generator {
+    return FromString(0, 0, 1, 1, ".")
+}
+
+func testCollidesOneElem(collisionTest func(Bitmask, Bitmask)bool, t *testing.T) {
+    f := func(b Bitmask) bool {
+        for y := 0; y<b.h; y++ {
+            for x := 0; x < b.w; x++ {
+                oneEl := FromString(b.X + x, b.Y + y, 1, 1, "#")
+                if b.rel(x, y) != b.dumbCollides(oneEl) {
+                    fmt.Printf("failed at %d, %d, %v %v:\n", x, y, b.rel(x, y),  b.dumbCollides(oneEl))
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
